c8374cd23f6a3c64ff6f8be904b381611f08231c
Montana <montanawong@gmail.com>
2016-04-09 14:46:14 -0400

    adding allowed terms

---
+++ AskMacysExtension/js/allowedSearchTerms.js
@@ -0,0 +1,19 @@
+/**
+ * Created by montanawong on 4/9/16.
+ */
+
+var allowedWords = array(
+    'dress',
+    'shirt',
+    'jacket',
+    'jeans',
+    'shoes',
+    'sandals',
+    'footwear'
+);
+
+var adjectives = array('leather', 'classic', 'suede');
+
+var colors = array('red', 'green', 'blue', 'black', 'white', 'yellow','purple', 'gray', );
+
+var genders = array('men', 'women', 'man', 'woman');
